ce88f4ae6edeea878e301c7e96a98219a168749d
HipLiv <96388870+HipLiv@users.noreply.github.com>
2022-03-23 20:23:16 +0100

    added some css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="Search Logo" />
+          <h1>Dictionary</h1>
+          <em>
+            <h4>Search for a Word</h4>
+          </em>
         </header>
         <main>
           <Dictionary />
