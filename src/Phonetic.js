4c3b21cf98f8e416872b7b514a7b70516297f5b8
HipLiv <96388870+HipLiv@users.noreply.github.com>
2022-03-24 20:02:20 +0100

    styled the phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,10 +4,8 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank">
-        Listen
+        {props.phonetic.text}
       </a>
-      <br />
-      {props.phonetic.text}
     </div>
   );
 }
