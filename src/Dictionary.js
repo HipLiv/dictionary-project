d03be6160fd7028d8407a3ef0599f670876df66e
HipLiv <96388870+HipLiv@users.noreply.github.com>
2022-03-25 18:27:14 +0100

    added photos for keywords

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let pexelsApiKey =
+      "563492ad6f917000010000012f720693c63b4159b22227544f8d625e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -29,6 +42,7 @@ export default function Dictionary() {
         <div className="hint">You could look for cake, yoga, push...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
